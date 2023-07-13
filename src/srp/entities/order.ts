6da2dc5d5c237be12d447a80b24516498e5e107a
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-07-13 17:20:53 -0300

    Single Responsability Principle

---
+++ src/srp/entities/order.ts
@@ -0,0 +1,33 @@
+import { OrderStatus } from './interfaces/ordem-status';
+import { Messaging } from '../services/messaging';
+import { ShoppingCart } from './shopping-cart';
+import { Persistency } from '../services/persistency';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+  ) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkOut(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this.messaging.sendMessage(
+      `Seu pedido com total de ${this.cart.total()} foi recebido!`,
+    );
+
+    this._orderStatus = 'closed';
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+}
