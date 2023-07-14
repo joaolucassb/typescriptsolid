52d6a78cd62b6d50075055b1139caec6417434ef
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-07-14 17:50:24 -0300

    Interface Segragation Principle

---
+++ src/isp/main.ts
@@ -0,0 +1,46 @@
+/*
+Interface segragation principle (Princípio da segregação de interface) -
+Os clientes não devem ser forçados a depender de type, interfaces ou membros
+abstratos que não utilizam
+*/
+
+import { ShoppingCart } from './classes/shopping-cart';
+import { Order } from './classes/order';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { NoDiscount } from './classes/discount';
+import { EnterpriseCustomer } from './classes/customer';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+// const individualCustomer = new IndividualCustomer(
+//   'João',
+//   'Lucas',
+//   '111.111.111-11',
+// );
+const enterpriseCustomer = new EnterpriseCustomer(
+  'Empresa LTDA',
+  '222222222222',
+);
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  enterpriseCustomer,
+);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.91223));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkOut();
+console.log(order.orderStatus);
