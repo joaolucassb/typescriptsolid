00c3131d10e865b80a5acce03cdc4e13e0465df6
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-07-14 15:58:09 -0300

    Open Closed Principle

---
+++ src/ocp/main.ts
@@ -0,0 +1,30 @@
+/*
+Open/Closed Principle
+Entidades devem estar abertas para extensão, mas fechadas para modificação.
+*/
+
+import { ShoppingCart } from './classes/shopping-cart';
+import { Order } from './classes/order';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { NoDiscount } from './classes/discount';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.91223));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkOut();
+console.log(order.orderStatus);
